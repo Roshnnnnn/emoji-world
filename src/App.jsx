43555a3d3603afeade64fbb71a6bc61faf9cfbe8
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-07-28 21:44:52 +0530

    working on emoji world

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+const emojiDictionary = {
+	"😊": "Smiling",
+	"😮": "Disbelief",
+	"😔": "Sad",
+	"🥡": "Takeout-box",
+	"❤️": "Heart",
+	"😑": "Annoyance",
+};
+
+export default function App() {
+	const [emojiMeaning, setEmojiMeaning] = useState("");
+
+	const emojiInput = (e) => {
+		var inputChange = e.target.value;
+		var emojiMeaning = emojiDictionary[inputChange];
+
+		if (emojiMeaning === undefined) {
+			emojiMeaning = "Sorry this emoji is not available in our emoji world";
+		}
+
+		setEmojiMeaning(emojiMeaning);
+	};
+
 	return (
-		<div>
-			<div></div>
+		<div className="App">
+			<h1>Inside Outtt !</h1>
+			<input onChange={emojiInput}></input>
+			<h2>{emojiMeaning}</h2>
 		</div>
 	);
 }
-
-export default App;
